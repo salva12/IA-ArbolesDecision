cf5b0f851c0983e4b22cba4230009189e95d0730
salva <salvamanu12@gmail.com>
2021-09-05 20:59:45 -0300

    estructura del proyecto

---
+++ dibujarMatriz.js
@@ -0,0 +1,7 @@
+function dibujarMatriz() {
+    console.log("ejecutandose dibujar matriz")
+    document.getElementById("matrizCarga").innerHTML= getElementById("filas") + getElementById("columnas")
+    return 
+}
+
+console.log("resultadosss...inprogress");
