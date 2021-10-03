d24e81b08cd4b571cad37cd659e173abca71e29a
Emmanuel Garcia <egarcia@kelsus.com>
2021-10-02 22:29:51 -0300

    adding navigation buttons

--- ia-arbolesdecision/src/App.js
+++ ia-arbolesdecision/src/App.js
@@ -5,10 +5,26 @@ import Data from './containers/Data';
 const App = props => {
   const [tabIndex, setTabIndex] = useState(0);
 
+  const onPrevious = () => {
+    if (tabIndex > 0) {
+      setTabIndex(tabIndex - 1);
+    }
+  };
+
+  const onNext = () => {
+    if (tabIndex < 2) {
+      setTabIndex(tabIndex + 1);
+    }
+  };
+
   return (
     <div className="App">
       {tabIndex === 0 && <Attributes />}
       {tabIndex === 1 && <Data />}
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+        <button onClick={onPrevious}>&#8592; Anterior</button>
+        <button onClick={onNext}>&#8594; Siguiente</button>
+      </div>
     </div>
   );
 }
