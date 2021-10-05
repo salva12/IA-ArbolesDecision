dbe4a408d00f261da4ec5890eb57627727f68e2a
salva <salvamanu12@gmail.com>
2021-10-05 10:48:22 -0300

    agregue calculo de la entropia de un atributo dado

--- C45.js
+++ C45.js
@@ -38,15 +38,7 @@ const mockdata = [
     }
     ];
 
-//el siguiente bloque sirve para saber de que tamaño son los registros, el campo de la clase estará ubicado en tamReg-1.
-const tamReg = Object.keys(mockdata[1]).length;
-const indiceCampoClase = tamReg - 1;
-console.log(indiceCampoClase);
-
-//el siguiente bloque sirve para saber cuales son los atributos y cual es la clase para decidir.
-const atributos = Object.keys(mockdata[1]);
-const clase = atributos.pop();
-console.log("los atts son " + atributos + "y la clase es:" + clase);
+// A CONTINUACIÓN SOLUCION GENERAL
 
 //el siguiente bloque sirve para saber la cantidad de valores diferentes que puede tomar un atributo o clase
 const valoresDistintos = (attClase,dataset) => {
@@ -73,8 +65,20 @@ function contarValores(valor,dataset,attClase){
     };
     return cont;
 };
-// funcion flecha para calcular la entropia global del set de datos D.
-const entropiaD = (dataset) => {
+
+// la siguiente funcion cuenta la cantidad de reg que pertenecen a un valor de clase y de atributo dado
+function contarValores2(valorAtt,valorClase,dataset,atributo,clase){
+    let cont = 0;
+    for (reg of dataset){
+        if (valorAtt == reg[atributo] & valorClase == reg[clase]){
+            cont++;
+        };
+    };
+    return cont;
+};
+
+// funcion para calcular la entropia global del set de datos D.
+const entropiaD = (dataset,clase) => {
     let valoresClase = valoresDistintos(clase,dataset);
     let entD = 0;
     for (valor of valoresClase){
@@ -84,7 +88,51 @@ const entropiaD = (dataset) => {
     return entD;
 };
 
-entropiaD(mockdata);
-console.log(valoresDistintos(clase,mockdata));
-console.log(contarValores("regular",mockdata,clase));
-console.log(entropiaD(mockdata));
+// funcion para calcular la entropia de un atributo respecto a D.
+const entropiaA = (dataset,atributo,clase) => {
+    let valoresAtributo = valoresDistintos(atributo,dataset);
+    let valoresClase = valoresDistintos(clase,dataset);
+    let entA = 0;
+    for (valor of valoresAtributo){
+        let denominador = contarValores(valor, dataset, atributo); 
+        let sumatoria1 = 0;
+        for (valorclase of valoresClase){
+            let numerador = contarValores2(valor,valorclase,dataset,atributo,clase);
+            if (numerador != 0 & denominador !=0){ //este control es porque no se calcula el log de 0, da math error, ni mucho menos de infinito
+                sumatoria1 = -(numerador/denominador)*logBase(2,numerador/denominador) + sumatoria1; 
+            }
+            else if(denominador==0){ //cuando el denominador es 0, no hay registros de un determinado valor de un atributo disponibles en el dataset actual, entonces la entropia es 0
+                sumatoria1 = sumatoria1 + 0;
+            };
+        };
+        if (denominador != 0){
+            entA = entA + sumatoria1*(denominador/dataset.length);
+        };
+    };
+    return entA;
+};
+
+//particularizando la solucion y probando resultados --> ACA HAY QUE GENERALIZAR ASIGNANDO A DATASER OTRA LO QUE TOMEMOS DE LO INGRESADO POR EL USUARIO
+dataset = mockdata;
+
+//el siguiente bloque sirve para saber cuales son los atributos y cual es la clase para decidir.
+const atributos = Object.keys(dataset[1]);
+const clase = atributos.pop();
+console.log("los atts son " + atributos + "y la clase es:" + clase);
+
+// Ver por consola los resultados parciales y pruebas de cada funcion
+entropiaD(dataset);
+
+console.log("Probando obtener valores distintos y contar valores de programa")
+console.log(valoresDistintos("programa",dataset));
+console.log(contarValores("no",dataset,"programa"));
+
+console.log("Probando entropia de D")
+console.log(entropiaD(dataset,"condicioncurso"));
+console.log("Probando entropia de programa")
+console.log(entropiaA(dataset,"programa","condicioncurso"));
+console.log("Probando entropia de menos de 3 inasistencias")
+console.log(entropiaA(dataset,"menos3inasis","condicioncurso"));
+console.log("Probando entropia de formacion secundaria")
+console.log(entropiaA(dataset,"formsec","condicioncurso"));
+
