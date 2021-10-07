3b88d9a99bb9562f14b867e295b39b1e970a6eab
salva <salvamanu12@gmail.com>
2021-10-06 23:40:14 -0300

    c45 parcial

--- C45.js
+++ C45.js
@@ -56,7 +56,7 @@ function logBase(x, num) {
 };
 
 // la siguiente funcion cuenta valores de una clase o atributo dado en el dataset
-function contarValores(valor,dataset,attClase){
+function contarValores(valor,dataset,attClase){ // attclase quiere decir atributo o clase 
     let cont = 0;
     for (reg of dataset){
         if (valor == reg[attClase]){
@@ -70,7 +70,7 @@ function contarValores(valor,dataset,attClase){
 function contarValores2(valorAtt,valorClase,dataset,atributo,clase){
     let cont = 0;
     for (reg of dataset){
-        if (valorAtt == reg[atributo] & valorClase == reg[clase]){
+        if (valorAtt == reg[atributo] && valorClase == reg[clase]){
             cont++;
         };
     };
@@ -98,10 +98,10 @@ const entropiaA = (dataset,atributo,clase) => {
         let sumatoria1 = 0;
         for (valorclase of valoresClase){
             let numerador = contarValores2(valor,valorclase,dataset,atributo,clase);
-            if (numerador != 0 & denominador !=0){ //este control es porque no se calcula el log de 0, da math error, ni mucho menos de infinito
+            if (numerador != 0 && denominador !=0){ //este control es porque no se calcula el log de 0, da math error, ni mucho menos de infinito
                 sumatoria1 = -(numerador/denominador)*logBase(2,numerador/denominador) + sumatoria1; 
             }
-            else if(denominador==0){ //cuando el denominador es 0, no hay registros de un determinado valor de un atributo disponibles en el dataset actual, entonces la entropia es 0
+            else if(denominador == 0){ //cuando el denominador es 0, no hay registros de un determinado valor de un atributo disponibles en el dataset actual, entonces la entropia es 0
                 sumatoria1 = sumatoria1 + 0;
             };
         };
@@ -111,14 +111,6 @@ const entropiaA = (dataset,atributo,clase) => {
     };
     return entA;
 };
-
-//particularizando la solucion y probando resultados --> ACA HAY QUE GENERALIZAR ASIGNANDO A DATASER OTRA LO QUE TOMEMOS DE LO INGRESADO POR EL USUARIO
-dataset = mockdata;
-
-//el siguiente bloque sirve para saber cuales son los atributos y cual es la clase para decidir.
-const atributos = Object.keys(dataset[1]);
-const clase = atributos.pop();
-console.log("los atts son " + atributos + "y la clase es:" + clase);
 
 // funcion para calcular la ganancia
 function gain(entropiaD,entropiaA){
@@ -136,6 +128,71 @@ function gainRatio(gainA,dataset,atributo){
     };
     g = gainA/(denominador);
     return g;
+};
+
+//particularizando la solucion y probando resultados --> ACA HAY QUE GENERALIZAR ASIGNANDO A DATASET OTRA LO QUE TOMEMOS DE LO INGRESADO POR EL USUARIO
+dataset = mockdata;
+
+//el siguiente bloque sirve para saber cuales son los atributos y cual es la clase para decidir.
+const atributos = Object.keys(dataset[0]);
+const clase = atributos.pop();
+console.log("los atts son " + atributos + "y la clase es:" + clase);
+
+// C45 algoritmo recursivo, antes de arrancar, y que es utilizable en todas las recursiones es importante saber la clase 
+//y naturalmente para la primer llamada al algoritmo los atributos
+var nodes = [];
+var edges = [];
+const umbral = 0.1;
+let id_nodes = 0;
+
+function c45(dataset,atributos,nodes,edges){
+    console.log(dataset,atributos);
+    console.log(nodes);
+    console.log(edges);
+    if (valoresDistintos(clase,dataset).length == 1){
+        id_nodes++;
+        nodes.push({id:id_nodes+dataset[0][clase],label:dataset[0][clase]}); //colocar nodos hojas
+    }
+    else if(atributos.length == 0){
+        let valoresClase = valoresDistintos(clase,dataset);
+        let cantClase = [];
+        for (valor of valoresClase){
+            let c = contarValores(valor,dataset,clase);
+            cantClase.push({valorClase:valor,cant:c});
+        };
+    }
+    else{
+        let entD = entropiaD(dataset,clase);
+        let ganancias = [];
+        for (atributo of atributos){
+            ganancias.push(gain(entD,entropiaA(dataset,atributo,clase)));
+        };
+        const maxgain = Math.max.apply(null,ganancias);
+        if (maxgain<umbral){
+            t = dataset[0][clase];
+        }
+        else{
+            ag = atributos[ganancias.indexOf(maxgain)]; // estoy seleccionando al nodo que mejor clasifica o reduce mas la impureza
+            console.log("el nodo que mas reduce impureza es:" + ag);
+            const valdist = valoresDistintos(ag,dataset);
+            console.log(valoresDistintos(ag,dataset));
+            const particiones = [];
+            for (valor of valdist){
+                const particion = dataset.filter(reg => reg[ag]==valor);
+                particiones.push(particion);
+            };
+            id_nodes++;
+            let idpadre = id_nodes+ag;
+            nodes.push({id:idpadre,label:ag});
+            for (particion of particiones){
+                console.log(particion);
+                if (particion.length != 0){
+                    edges.push({from:idpadre,label:particion[0][ag]});
+                    c45(particion,atributos.filter(a => a != ag),nodes,edges);
+                };
+            };
+        };
+    };
 };
 
 // Ver por consola los resultados parciales y pruebas de cada funcion
@@ -163,3 +220,10 @@ const gainprograma = gain(entropiaD(dataset,"condicioncurso"),entropiaA(dataset,
 
 console.log("Probando la tasa de ganancia de la informacion para att programa");
 console.log(gainRatio(gainprograma,dataset,"programa"));
+
+// Aux sirve para saber el num maximo en un array
+console.log(Math.max.apply(null,[2,4,5,6,7,1,2,3])); 
+
+//Probamos el algoritmo completo
+console.log(c45(dataset,atributos,nodes,edges));
+
