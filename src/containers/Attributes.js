50d93a9b0228f4ee7ea7139b995c09c49239a83a
Emmanuel Garcia <egarcia@kelsus.com>
2021-10-30 21:06:35 -0300

    adding some comments

--- src/containers/Attributes.js
+++ src/containers/Attributes.js
@@ -4,18 +4,25 @@ import { ATTRIBUTE_TEMPLATE } from "../utils/constants";
 import { ReactComponent as Plus } from '../assets/plus-solid.svg';
 
 const Attributes = ({ attributes, setAttributes, data, setData }) => {
+  // function for adding an attribute
   const onAddAttribute = () => {
     setAttributes([...attributes, ATTRIBUTE_TEMPLATE]);
   };
 
+  // function for editing an attribute
   const onEditName = (index, newName) => {
+    // get the name of the attribute
     const oldAttributeName = attributes[index].label;
+    // spread the attributes array to avoid mutating props
     const newAttributes = [...attributes];
+    // replace the target attribute with a copy of it with the new name
     newAttributes.splice(index, 1, {
       ...attributes[index],
       label: newName,
     });
+    // update the attributes state
     setAttributes(newAttributes);
+    // in the data array, replace every old-named key with the new name
     setData(data.map(d => {
       const newD = {
         ...d,
@@ -26,17 +33,26 @@ const Attributes = ({ attributes, setAttributes, data, setData }) => {
     }));
   };
 
+  // function for editing an attribute's value
   const onEditValue = (index, valueIndex, newValue) => {
+    // get the name of the attribute whose value will be changed
     const attributeWithChangedValue = attributes[index].label;
+    // get the old name of the value to be changed
     const changedValue = attributes[index].values[valueIndex];
+    // spread the attributes array to avoid mutating props
     const newAttributes = [...attributes];
+    // spread the values array of the attribute
     const newValues = [...attributes[index].values];
+    // remove the old value and replace it with the new one
     newValues.splice(valueIndex, 1, newValue);
+    // replace the attribute with a copy of it with the updated values
     newAttributes.splice(index, 1, {
       ...attributes[index],
       values: newValues,
     });
+    // update the attributes state
     setAttributes(newAttributes);
+    // in the data array, replace the changed value with the new one
     setData(data.map(d => {
       const newD = { ...d };
       if (newD[attributeWithChangedValue] === changedValue) {
@@ -46,24 +62,35 @@ const Attributes = ({ attributes, setAttributes, data, setData }) => {
     }));
   };
 
+  // function for adding a new value to an attribute
   const onAddValue = index => {
+    // spread the attributes array to avoid mutating props
     const newAttributes = [...attributes];
+    // replace the attribute with a copy of it with a new empty value
     newAttributes.splice(index, 1, {
       ...attributes[index],
-      values: [...attributes[index].values, ""],
+      values: [...attributes[index].values, ''],
     });
+    // update the attributes state
     setAttributes(newAttributes);
   };
 
+  // function for deleting a value
   const onDeleteValue = (index, valueIndex) => {
+    // get the label of the attribute
     const attributeWithDeletedValue = attributes[index].label;
+    // get the value that will be deleted
     const deletedValue = attributes[index].values[valueIndex];
+    // spread the attributes array to avoid mutating props
     const newAttributes = [...attributes];
+    // replace the attribute with a copy of it without the deleted value
     newAttributes.splice(index, 1, {
       ...attributes[index],
       values: attributes[index].values.filter((_v, i) => i !== valueIndex),
     });
+    // update the attributes state
     setAttributes(newAttributes);
+    // in the data array, replace all the ocurrences of the deleted value with an empty string
     setData(data.map(d => {
       const newD = { ...d };
       if (newD[attributeWithDeletedValue] === deletedValue) {
@@ -73,10 +100,15 @@ const Attributes = ({ attributes, setAttributes, data, setData }) => {
     }));
   };
 
+  // function for deleting an attribute
   const onDeleteAttribute = index => {
+    // get the name of the attribute that will be deleted
     const deletedAttributeName = attributes[index].label;
+    // filter the attributes array excluding the deleted attribute
     const newAttributes = attributes.filter((_a, i) => i !== index);
+    // update the attributes state
     setAttributes(newAttributes);
+    // in the data array, delete all the properties of this attribute
     setData(data.map(d=> {
       const newD = { ...d };
       delete newD[deletedAttributeName];
