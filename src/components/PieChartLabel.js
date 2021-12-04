5123da0546c2131ff355493c479054dfae1c2275
Emmanuel Garcia <egarcia@kelsus.com>
2021-12-04 17:01:29 -0300

    adding the pie chart label component

---
+++ src/components/PieChartLabel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PieChartLabel = ({ label, value, ...rest }) => (
+  <text {...rest} style={{ fontSize: '8px' }}>
+    <tspan dy=".6em">
+      {label}
+    </tspan>
+    <tspan dy="1.2em">
+      {value}%
+    </tspan>
+  </text>
+);
+
+export default PieChartLabel;
