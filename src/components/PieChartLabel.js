42499b17e4688c93e1207e8bc6f6f82cf508f81b
Emmanuel Garcia <egarcia@kelsus.com>
2021-12-04 19:20:39 -0300

    decreasing the font size

--- src/components/PieChartLabel.js
+++ src/components/PieChartLabel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PieChartLabel = ({ label, value, ...rest }) => (
-  <text {...rest} style={{ fontSize: '8px' }}>
+  <text {...rest} style={{ fontSize: '6px' }}>
     <tspan dy=".6em">
       {label}
     </tspan>
